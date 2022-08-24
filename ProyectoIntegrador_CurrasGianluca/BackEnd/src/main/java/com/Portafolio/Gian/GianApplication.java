46d1992f62adb5c3d4fff9062058dad7de35c533
GGCRV <99265289+GGCRV@users.noreply.github.com>
2022-08-24 17:18:10 -0300

    .
    .

---
+++ ProyectoIntegrador_CurrasGianluca/BackEnd/src/main/java/com/Portafolio/Gian/GianApplication.java
@@ -0,0 +1,13 @@
+package com.Portafolio.Gian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianApplication.class, args);
+	}
+
+}
